6913c52ed4c4ea4f14de144d1b6e529304647f17
rohitgr7 <rohitgr1998@gmail.com>
2022-03-01 16:26:40 +0530

    update for failing test

--- pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -423,6 +423,7 @@ class AcceleratorConnector:
         if self._devices_flag in ([], 0, "0"):
             rank_zero_warn(f"You passed `devices={devices}`, switching to `cpu` accelerator")
             self._accelerator_flag = "cpu"
+            self._devices_flag = 1
 
         if self._devices_flag == "auto" and self._accelerator_flag is None:
             raise MisconfigurationException(
